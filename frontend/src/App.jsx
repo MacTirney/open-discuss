d346de1a6e5b4b6c0fcce1eb3836c92621f8999e
Liam Tirney <liamtirney7427@gmail.com>
2023-09-29 19:11:21 -0400

    Create DiscussionCard component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
+import DiscussionCard from './components/DiscussionCard.jsx'
 
 export default function App() {
     return (
         <>
             <Navbar />
+            <DiscussionCard />
         </>
     )
 }
---
+++ frontend/src/components/DiscussionCard.jsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+// import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+// import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+// import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+// import { red } from '@mui/material/colors';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+// import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+export default function DiscussionCard() {
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <Box sx={{ display: 'flex' }}>
+                <CardHeader
+                    title="Discussion Name"
+                />
+                <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </ExpandMore>
+            </Box>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Typography paragraph>Small excerpt per post...</Typography>
+                </CardContent>
+            </Collapse>
+        </Card>
+    );
+}
