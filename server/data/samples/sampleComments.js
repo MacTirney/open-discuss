b1e421e6a4c6e4c14710a34a4bb1ed1108307301
Mac Tirney <mactirney@gmail.com>
2023-09-30 23:03:53 -0400

    Add blank sample data files

---
+++ server/data/samples/sampleComments.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+      // userId: Number,
+      displayName: "", 
+      postId: "",
+      content: "This is what I did",
+      // time: Date
+    }
+]
---
+++ server/data/samples/sampleDiscussion.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    // userId: "",
+    // community: "",
+    postNumber: 1,
+    timestamp: new Date(),
+    title: "How can I get started with contributing to open source?",
+    content: "I'm new to the open source community and I'd like to \
+      get anyone's advice on how to get started.",
+    commentIds: []
+  },
+  {
+    // userId: "",
+    // community: "",
+    postNumber: 2,
+    timestamp: new Date(),
+    title: "List of open source communities",
+    content: "For anyone who is trying to join open source communities, \
+      here is a list of communities you can join",
+    commentIds: []
+  },
+  {
+    // userId: "",
+    // community: "",
+    postNumber: 3,
+    timestamp: new Date(),
+    title: "How do I make a pull request?",
+    content: "For anyone who is trying to join open source communities, \
+      here is a list of communities you can join",
+    commentIds: []
+  },
+  {
+    // userId: "",
+    // community: "",
+    postNumber: 4,
+    timestamp: new Date(),
+    title: "I have a merge conflict I can't resolve",
+    content: "I'm trying to merge my branch into main, and I don't know how to resolve them. \n \
+      Here's more details: ... ",
+    commentIds: []
+  }
+]
