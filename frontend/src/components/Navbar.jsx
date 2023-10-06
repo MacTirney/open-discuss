0a1bd1f9b8d07535053d084c45b58552db7b92e8
Liam Tirney <liamtirney7427@gmail.com>
2023-10-05 20:04:51 -0400

    Map over navbar buttons and clean up styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,33 +4,20 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import logo from '../assets/communication-logo.png'
 
-export default function Navbar() {
-  let navigate = useNavigate();
+const pages = ['discuss', 'communities', 'resources', 'login'];
 
-  // function handleClick(path) {
-  //   navigate.push(path);
-  // }
+const Navbar = () => {
+  let navigate = useNavigate();
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
           <Box
             component="img"
             sx={{ height: 54 }}
@@ -38,7 +25,6 @@ export default function Navbar() {
             src={logo}
           >
           </Box>
-          {/* <img src={logo} /> */}
           <Typography
             variant="h6"
             component="div"
@@ -46,44 +32,19 @@ export default function Navbar() {
           >
             Open Discuss
           </Typography>
-          <Button 
-            color="inherit"
-            onClick={() => navigate("/discuss")}
-          >
-            Discuss
-          </Button>
-          <Button 
-            color="inherit"
-            onClick={() => navigate("/communities")}
-          >
-            Communities
-          </Button>
-          <Button 
-            color="inherit"
-            onClick={() => navigate("/resources")}
-          >
-            Resources
-          </Button>
-          <Button 
-            color="inherit"
-            onClick={() => navigate("/login")}
-          >
-            Login
-          </Button>
+          {pages.map((page) => (
+            <Button 
+              key={page}
+              color="inherit"
+              onClick={() => navigate(`/${page}`)}
+            >
+              {page}
+            </Button>
+          ))}
         </Toolbar>
       </AppBar>
     </Box>
-    // <nav className="nav">
-    //     <div className="nav--left">
-    //         <img className="nav--logo" src={computerLogo} />
-    //         <h3>Open Discuss</h3>
-    //     </div>
-    //     <div className="nav--right">
-    //         <h4>Discussions</h4>
-    //         <h4>Communities</h4>
-    //         <h4>Resources</h4>
-    //         <h4>Login</h4>
-    //     </div>
-    // </nav>
   )
 }
+
+export default Navbar;
