f983618c3f25b1f7e844b41be37430c17e63889f
Liam Tirney <liamtirney7427@gmail.com>
2023-10-05 20:07:01 -0400

    Uninstall dependencies

---
+++ frontend/test.js
@@ -0,0 +1,17 @@
+
+// line 18 Navbar.jsx
+// function handleClick(path) {
+  //   navigate.push(path);
+  // }
+// const navigateButton = () => {
+  //   return (pages.map((page) => (
+  //     <Button 
+  //       key={page}
+  //       color="inherit"
+  //       onClick={() => navigate(`/${page}`)}
+  //       // sx={{ color: 'white' }}
+  //     >
+  //       {page}
+  //     </Button>
+  //   )))
+  // }
